8835cea1ebe5c33d4afdc49d4b419645a7b30097
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-05-15 09:26:03 +0530

    Update c5-ec2instance.tf

--- c5-ec2instance.tf
+++ c5-ec2instance.tf
@@ -1,11 +1,26 @@
-# EC2 Instance
 resource "aws_instance" "myec2vm" {
   ami = data.aws_ami.amzlinux2.id
   instance_type = var.instance_type
-  user_data = file("${path.module}/app1-install.sh")
+  #user_data = file("${path.module}/app1-install.sh")
   key_name = var.instance_keypair
-  vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id   ]
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id]
   tags = {
     "Name" = "EC2 Demo 2"
+  }
+  provisioner "remote-exec" {
+    #inline = [
+    #  "touch /home/ec2-user/terraform-created.txt"
+    #]
+    script = "${path.module}/scripts/create-file.sh" 
+
+
+    connection{
+       type          = "ssh"
+       user          = "ec2-user"
+       private_key   = file("${path.module}/private-key/terraform-key.pem")
+       host          = self.public_ip
+
+    }
+
   }
 }
